48bbc94c7b276f7164460c2cb978a604806bc2fd
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-07-05 21:43:43 +0200

    only show given user's created inspirations

--- app/Http/Livewire/InspirationView.php
+++ app/Http/Livewire/InspirationView.php
@@ -10,7 +10,7 @@ class InspirationView extends Component
     public function render()
     {
         return view('livewire.inspiration-view', [
-            "inspirations" => Inspiration::paginate(2)->withQueryString()
+            "inspirations" => Inspiration::where("creator", auth()->user()->id)->paginate(2)->withQueryString()
         ]);
     }
 }
