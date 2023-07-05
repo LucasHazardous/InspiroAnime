7b732f291650c06b7837d1ed405f1f121d654c55
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-07-05 14:45:21 +0200

    createAndSaveImage different apis

--- app/Actions/InspirationGenerate.php
+++ app/Actions/InspirationGenerate.php
@@ -6,28 +6,31 @@ use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
 
 class InspirationGenerate {
-    public static function createAndSaveImage(string $limit): string {
-        $imgData = json_decode(file_get_contents("https://nekos.best/api/v2/waifu"));
-        $url = $imgData->results[0]->url;
+    public static function createAndSaveImage(string $limit, string $tag): array {
+        $imgData = json_decode(file_get_contents("https://api.waifu.im/search?included_tags=$tag&is_nsfw=false"));
+        $url = $imgData->images[0]->url;
+        $source = $imgData->images[0]->source;
 
         $info = pathinfo($url);
         $contents = file_get_contents($url);
         $file = '/tmp/' . $info['basename'];
         file_put_contents($file, $contents);
 
-        $img = imagecreatefrompng($file);
+        $extension = pathinfo($file, PATHINFO_EXTENSION);
+        $img = ($extension == 'jpeg' || $extension == 'jpg') ? imagecreatefromjpeg($file) : imagecreatefrompng($file);
 
-        $txt = json_decode(file_get_contents("https://animechan.xyz/api/random"))->quote;
+        $txt = substr(json_decode(file_get_contents("https://animechan.xyz/api/random"))->quote, 0, $limit);
         $fontFile = "./Roboto.ttf";
         $fontSize = imagesx($img)/$limit*1.5;
         $fontColor = imagecolorallocate($img, 255, 0, 0);
 
-        imagettftext($img, $fontSize, 0, 0, $fontSize, $fontColor, $fontFile, substr($txt, 0, $limit));
+        imagettftext($img, $fontSize, 0, 0, $fontSize, $fontColor, $fontFile, $txt);
 
         imagepng($img, $file);
 
         $uploaded_file = new UploadedFile($file, $info['basename']);
         
-        return Storage::disk("public")->put('images', $uploaded_file);
+        $imgPath = Storage::disk("public")->put('images', $uploaded_file);
+        return [$imgPath, $txt, $source];
     }
 }
