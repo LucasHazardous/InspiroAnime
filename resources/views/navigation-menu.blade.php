229466b98fb0612f9683139a3eb20d440baf92da
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-07-05 18:38:59 +0200

    navigation

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,6 +15,10 @@
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+
+                    <x-nav-link href="{{ route('inspirations.index') }}" :active="request()->routeIs('inspirations.index')">
+                        {{ __('Inspirations') }}
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -141,6 +145,9 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link href="{{ route('inspirations.index') }}" :active="request()->routeIs('inspirations.index')">
+                        {{ __('Inspirations') }}
             </x-responsive-nav-link>
         </div>
 
