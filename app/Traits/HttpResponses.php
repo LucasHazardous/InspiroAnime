a477be65e6c2b8886b788467565b0312fd3112c4
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-07-05 20:06:44 +0200

    HttpResponses trait

---
+++ app/Traits/HttpResponses.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\JsonResponse;
+
+trait HttpResponses {
+    protected function success($data, $message=null, $code=200): JsonResponse
+    {
+        return response()->json([
+            "status" => 'Request was successful.',
+            "message" => $message,
+            "data" => $data
+        ], $code);
+    }
+
+    protected function error($data, $message=null, $code=400): JsonResponse
+    {
+        return response()->json([
+            "status" => 'Error has occurred.',
+            "message" => $message,
+            "data" => $data
+        ], $code);
+    }
+}
