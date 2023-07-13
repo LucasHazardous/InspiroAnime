e1b92c15dee654afe239d9ed3f30bdc842a5909a
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-07-13 14:59:54 +0200

    inspiration view select fields and sort

--- app/Http/Livewire/InspirationView.php
+++ app/Http/Livewire/InspirationView.php
@@ -10,7 +10,11 @@ class InspirationView extends Component
     public function render()
     {
         return view('livewire.inspiration-view', [
-            "inspirations" => Inspiration::where("creator", auth()->user()->id)->paginate(2)->withQueryString()
+            "inspirations" => Inspiration::select("id", "creator", "created_at", "text", "source", "image")
+            ->where("creator", auth()->user()->id)
+            ->orderBy("created_at", "desc")
+            ->paginate(2)
+            ->withQueryString()
         ]);
     }
 }
